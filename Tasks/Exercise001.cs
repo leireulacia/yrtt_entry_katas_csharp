bdea394fecf32af50b072c9845b9993e4fa9450c
leireUlacia <leulaciah@gmail.com>
2020-11-09 22:57:43 +0000

    Implement exercise 1

--- Tasks/Exercise001.cs
+++ Tasks/Exercise001.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq; // To use the query
 
 // In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. 
 // Your task will be to return the sum of the numbers that occur only once.
@@ -14,7 +15,21 @@ namespace TechReturners.Tasks
     {
         public static int Singles(List<int> source)
         {
-            throw new NotImplementedException();
+            // throw new NotImplementedException();
+            int sum = 0;
+
+            // Query that groups by the identifier, and then filter for groups that have only one member      
+            var noDuplicates = source
+                              .GroupBy(i => i)
+                              .Where(g => g.Count() == 1) // There are only 1 element in the list
+                              .Select(g => g.Key);
+
+            foreach (int num in noDuplicates)
+            {
+                sum += num;
+            }
+
+            return (sum);
         }
     }
 }
